0ca0599e07c29cb680884d72cd277098d3358896
Cyril Moire <cyril.moire@atos.net>
2016-01-14 16:16:02 +0100

    TICKETING-12 : init data sample

---
+++ src/sql/plugins/workflow/plugin/init_db_workflow_data_sample_ticketing.sql
@@ -0,0 +1,4 @@
+INSERT INTO workflow_workflow VALUES (301,'Workflow GRU','Workflow GRU','2016-01-13 08:36:34',1,'all');
+
+INSERT INTO workflow_state VALUES (301,'Nouveau','Nouveau',301,1,0,NULL,1);
+INSERT INTO workflow_state VALUES (302,'A qualifier','A qualifier',301,0,0,NULL,2);
