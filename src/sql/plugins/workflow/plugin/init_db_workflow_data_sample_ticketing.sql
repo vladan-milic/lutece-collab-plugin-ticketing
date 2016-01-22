39853c1b045283e792af00c87de9262df1d430ad
Cyril Moire <cyril.moire@atos.net>
2016-01-22 09:42:49 +0100

    TICKETING-32 : add the task to qualify the ticket in the workflow

--- src/sql/plugins/workflow/plugin/init_db_workflow_data_sample_ticketing.sql
+++ src/sql/plugins/workflow/plugin/init_db_workflow_data_sample_ticketing.sql
@@ -2,3 +2,7 @@ INSERT INTO workflow_workflow VALUES (301,'Workflow GRU','Workflow GRU','2016-01
 
 INSERT INTO workflow_state VALUES (301,'Nouveau','Nouveau',301,1,0,NULL,1);
 INSERT INTO workflow_state VALUES (302,'A qualifier','A qualifier',301,0,0,NULL,2);
+
+INSERT INTO workflow_action VALUES (301,'Initialisation','Initialisation de la sollicitation',301,301,302,1,1,0,1,0);
+
+INSERT INTO workflow_task VALUES (301,'taskTicketingGenerateTicketReference',301,1);
