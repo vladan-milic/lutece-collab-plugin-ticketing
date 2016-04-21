b36439632b4e5814b80db7e903acf986a6ca28bc
Francois GandemerC:\FGR\Projets\Lutece\git <francois.gandemer@atos.net>
2016-04-21 08:58:48 +0200

    TICKETING-85 remove useless permissions

--- src/java/fr/paris/lutece/plugins/ticketing/service/TicketResourceIdService.java
+++ src/java/fr/paris/lutece/plugins/ticketing/service/TicketResourceIdService.java
@@ -64,42 +64,11 @@ public class TicketResourceIdService extends ResourceIdService
     /** Permission for deleting a ticket */
     public static final String PERMISSION_VIEW = "VIEW";
 
-    /** Permission for self assigning a ticket */
-    public static final String PERMISSION_SELF_ASSIGN = "SELF_ASSIGN";
-
-    /** Permission for assigning a ticket to a unit */
-    public static final String PERMISSION_ASSIGN_TO_UNIT = "ASSIGN_TO_UNIT";
-
-    /** Permission for assigning a ticket to a user */
-    public static final String PERMISSION_ASSIGN_TO_USER = "ASSIGN_TO_USER";
-
-    /** Permission for assigning up a ticket */
-    public static final String PERMISSION_ASSIGN_UP_LEVEL = "ASSIGN_UP_LEVEL";
-
-    /** Permission for qualifying a ticket */
-    public static final String PERMISSION_QUALIFY = "QUALIFY";
-
-    /** Permission for returnin a ticket to assigner */
-    public static final String PERMISSION_RETURN_TO_ASSIGNER = "RETURN_TO_ASSIGNER";
-
-    /** Permission for responding to assign up of a ticket */
-    public static final String PERMISSION_RESPOND_ASSIGN_UP_LEVEL = "RESPOND_ASSIGN_UP_LEVEL";
-
-    /** Permission for responding to a user for a ticket */
-    public static final String PERMISSION_RESPOND_TO_USER = "RESPOND_TO_USER";
     private static final String PROPERTY_LABEL_RESOURCE_TYPE = "ticketing.ticket.resourceType";
     private static final String PROPERTY_LABEL_CREATE = "ticketing.ticket.permission.label.create";
     private static final String PROPERTY_LABEL_MODIFY = "ticketing.ticket.permission.label.modify";
     private static final String PROPERTY_LABEL_DELETE = "ticketing.ticket.permission.label.delete";
     private static final String PROPERTY_LABEL_VIEW = "ticketing.ticket.permission.label.view";
-    private static final String PROPERTY_LABEL_SELF_ASSIGN = "ticketing.ticket.permission.label.selfAssign";
-    private static final String PROPERTY_LABEL_ASSIGN_TO_UNIT = "ticketing.ticket.permission.label.assignToUnit";
-    private static final String PROPERTY_LABEL_ASSIGN_TO_USER = "ticketing.ticket.permission.label.assignToUser";
-    private static final String PROPERTY_LABEL_ASSIGN_UP_LEVEL = "ticketing.ticket.permission.label.assignUpLevel";
-    private static final String PROPERTY_LABEL_QUALIFY = "ticketing.ticket.permission.label.qualify";
-    private static final String PROPERTY_LABEL_RETURN_TO_ASSIGNER = "ticketing.ticket.permission.label.returnToAssigner";
-    private static final String PROPERTY_LABEL_RESPOND_TO_USER = "ticketing.ticket.permission.label.respondToUser";
-    private static final String PROPERTY_LABEL_RESPOND_ASSIGN_UP_LEVEL = "ticketing.ticket.permission.label.respondAssignUpLevel";
 
     /**
      * Constructor
@@ -138,46 +107,6 @@ public class TicketResourceIdService extends ResourceIdService
         p = new Permission(  );
         p.setPermissionKey( PERMISSION_VIEW );
         p.setPermissionTitleKey( PROPERTY_LABEL_VIEW );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_SELF_ASSIGN );
-        p.setPermissionTitleKey( PROPERTY_LABEL_SELF_ASSIGN );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_ASSIGN_TO_UNIT );
-        p.setPermissionTitleKey( PROPERTY_LABEL_ASSIGN_TO_UNIT );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_ASSIGN_TO_USER );
-        p.setPermissionTitleKey( PROPERTY_LABEL_ASSIGN_TO_USER );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_ASSIGN_UP_LEVEL );
-        p.setPermissionTitleKey( PROPERTY_LABEL_ASSIGN_UP_LEVEL );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_QUALIFY );
-        p.setPermissionTitleKey( PROPERTY_LABEL_QUALIFY );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_RETURN_TO_ASSIGNER );
-        p.setPermissionTitleKey( PROPERTY_LABEL_RETURN_TO_ASSIGNER );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_RESPOND_TO_USER );
-        p.setPermissionTitleKey( PROPERTY_LABEL_RESPOND_TO_USER );
-        rt.registerPermission( p );
-
-        p = new Permission(  );
-        p.setPermissionKey( PERMISSION_RESPOND_ASSIGN_UP_LEVEL );
-        p.setPermissionTitleKey( PROPERTY_LABEL_RESPOND_ASSIGN_UP_LEVEL );
         rt.registerPermission( p );
 
         ResourceTypeManager.registerResourceType( rt );
