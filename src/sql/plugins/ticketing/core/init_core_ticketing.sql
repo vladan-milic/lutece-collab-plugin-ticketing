b9a2df2478c9c95dd6843da0dca81a3374320cd8
Cyril Moire <cyril.moire@atos.net>
2016-05-17 15:51:25 +0200

    TICKETING-32 : do not remove the right for instant responses for users

--- src/sql/plugins/ticketing/core/init_core_ticketing.sql
+++ src/sql/plugins/ticketing/core/init_core_ticketing.sql
@@ -76,7 +76,7 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 --
 -- Data for table core_user_right
 --
-DELETE FROM core_user_right WHERE id_right = 'TICKETING_INSTANT_RESPONSE_MANAGEMENT';
+DELETE FROM core_user_right WHERE id_right = 'TICKETING_INSTANT_RESPONSE_MANAGEMENT' AND id_user = 1;
 INSERT INTO core_user_right (id_right,id_user) VALUES ('TICKETING_INSTANT_RESPONSE_MANAGEMENT',1);
 
 --
