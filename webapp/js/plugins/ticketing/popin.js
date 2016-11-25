d8aa079202d99612582a6079c8bd8249dfc392cb
ATOS-NTY <nicolas.tanqueray@atos.net>
2016-11-25 10:11:17 +0100

    TICKETING-208 - Data conservation in popin ticketing

--- webapp/js/plugins/ticketing/popin.js
+++ webapp/js/plugins/ticketing/popin.js
@@ -1,7 +1,20 @@
 function loadModalContent(modalSelector){
 	$(modalSelector).on("show.bs.modal", function(e) {
 	    var link = $(e.relatedTarget);
-	    $(this).find(".modal-body").load(link.attr("href"));
-	    $(this).find(".modal-title").text( link.data('title') );
+		var attrActionId = $(this).attr("aria-action-id");
+		var linkActionId = link.attr("data-action-id");
+		if ( typeof attrActionId === typeof undefined || attrActionId === false )
+		{
+			attrActionId = "";
+		}
+		if( attrActionId == "" || linkActionId != attrActionId )
+		{
+			$(this).find(".modal-body").load( link.attr("href") );
+			$(this).find(".modal-title").text( link.data('title') );
+			if ( typeof linkActionId !== typeof undefined && linkActionId !== false )
+			{
+				$(this).attr( "aria-action-id", linkActionId );
+			}
+		}
 	});
 }
