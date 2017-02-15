ff36e35cf0daad4ff5ba430d559b69666e5c7e23
AlexandreClose <alexandre.close@gmail.com>
2017-02-15 11:53:12 +0100

    TICKETING-242 : Change right level for TICKETING_USER_PREFERENCES from 1 to 3

--- src/sql/plugins/ticketing/core/init_core_ticketing.sql
+++ src/sql/plugins/ticketing/core/init_core_ticketing.sql
@@ -40,7 +40,7 @@ INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is
 
 DELETE FROM core_admin_right WHERE id_right = 'TICKETING_USER_PREFERENCES_MANAGEMENT';  
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('TICKETING_USER_PREFERENCES_MANAGEMENT','ticketing.adminFeature.ManageUserPreferences.name',1,'jsp/admin/plugins/ticketing/UserPreferences.jsp','ticketing.adminFeature.ManageUserPreferences.description',0,'ticketing','TICKETING',NULL,NULL,3);
+('TICKETING_USER_PREFERENCES_MANAGEMENT','ticketing.adminFeature.ManageUserPreferences.name',3,'jsp/admin/plugins/ticketing/UserPreferences.jsp','ticketing.adminFeature.ManageUserPreferences.description',0,'ticketing','TICKETING',NULL,NULL,3);
 
 --
 -- Data for table core_user_right
