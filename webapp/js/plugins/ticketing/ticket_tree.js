92055623115961224a393d0444cc1ca829aa702a
TrainE <erwan.train@atos.net>
2017-03-30 15:51:49 +0200

    TICKETING-269 : Update the list of type/domain/category for the creation of a ticket with default value

--- webapp/js/plugins/ticketing/ticket_tree.js
+++ webapp/js/plugins/ticketing/ticket_tree.js
@@ -1,3 +1,8 @@
+// The three messages of selections
+var msg_select_type = "-- Choisir une nature --";
+var msg_select_domain = "-- Choisir un domaine --";
+var msg_select_category = "-- Choisir une probl\u00e9matique --";
+
 // Turns the 3 combos identified by the jquery selectors into dynamic combos
 function lutece_ticket_tree(type_selector, domain_selector, category_selector, precision_selector, selected_category_id, url) {
     var base = $('head base').attr('href');
@@ -45,12 +50,25 @@ function lutece_ticket_tree(type_selector, domain_selector, category_selector, p
 						}
                     }
                 }
+                var emptyCategory = {};
+                emptyCategory['id'] = -1;
+                emptyCategory['label'] = msg_select_category;
+                domain.categories.unshift(emptyCategory);
             }
+            var emptyDomain = {};
+            emptyDomain['id'] = -1;
+            emptyDomain['label'] = msg_select_domain;
+            type.domains.unshift(emptyDomain);
         }
+        var emptyType = {};
+        emptyType['id'] = -1;
+        emptyType['label'] = msg_select_type;
+        data.types.unshift(emptyType);
+        
 		if (selectedType == undefined) {
 			selectedType = data.types[0];
-			selectedDomain = selectedType.domains[0];
-			selectedCategory = selectedDomain.categories[0];
+			selectedDomain = data.types[1].domains[0];
+			selectedCategory = data.types[1].domains[1].categories[0];
 			if (selectedCategory.precisions != undefined) {
 				selectedPrecision = selectedCategory.precisions[0];
 			}
@@ -58,7 +76,7 @@ function lutece_ticket_tree(type_selector, domain_selector, category_selector, p
 
 
         var load_messages = function() {
-        	if (selectedCategory.help != undefined) {
+        	if (selectedCategory!= undefined && selectedCategory.help != undefined) {
         		$("#help_message_category").html(selectedCategory.help);
         	} else {
         		$("#help_message_category").html("");
@@ -87,26 +105,41 @@ function lutece_ticket_tree(type_selector, domain_selector, category_selector, p
 		loadGenericAttributesForm(url, false, category_selector, true);
 
 		$(type_selector).change(function() {
+			if($(type_selector).val() != '-1' && $(type_selector).find('option[value="-1"]').length > 0 )
+			{
+				document.getElementById("id_ticket_type").remove(0);
+			}
 			selectedType = types_map[$(type_selector).val()];
 			load_combo(domain_selector, selectedType.domains);
 			$(domain_selector).change();
         });
 
         $(domain_selector).change(function() {
+			if($(domain_selector).val() != '-1' && $(domain_selector).find('option[value="-1"]').length > 0 )
+			{
+				document.getElementById("id_ticket_domain").remove(0);
+			}
 			selectedDomain = selectedType.domains_map[$(domain_selector).val()];
-			load_combo(category_selector, selectedDomain.categories);
+			load_combo(category_selector, (selectedDomain == undefined ? selectedDomain : selectedDomain.categories), -1, msg_select_category);
 			$(category_selector).change();
         });
 
         $(category_selector).change(function() {
-        	selectedCategory = selectedDomain.categories_map[$(category_selector).val()];
-			load_combo(precision_selector, selectedCategory.precisions);
+			if($(category_selector).val() != '-1' && $(category_selector).find('option[value="-1"]').length > 0 )
+			{
+				document.getElementById("id_ticket_category").remove(0);
+			}
+			selectedCategory = (selectedDomain != undefined ? selectedDomain.categories_map[$(category_selector).val()] : selectedDomain);
+			load_combo(precision_selector, (selectedCategory == undefined ? selectedCategory : selectedCategory.precisions));
 			$(precision_selector).change();
 			
         });
 
         $(precision_selector).change(function() {
-        	selectedPrecision = selectedCategory.precisions_map[$(precision_selector).val()];
+			if(selectedCategory != undefined && selectedCategory.precisions_map != undefined)
+			{
+				selectedPrecision = selectedCategory.precisions_map[$(precision_selector).val()];
+			}
 			load_messages();
 			setSelectedCategoryId();
 			loadGenericAttributesForm(url, false, category_selector, false);
@@ -136,7 +169,10 @@ function loadGenericAttributesForm(url, is_response_reseted, category_selector, 
 					{
 						positionToScroll = $('.alert.alert-danger').parent('.form-group').offset().top;
 					}
-					
+					else 
+					{
+						positionToScroll = $('.alert.alert-danger').closest('.form-group').offset().top;
+					}					
 					$('html,body').animate({
 						  scrollTop: positionToScroll
 						}, 1000);
@@ -146,17 +182,27 @@ function loadGenericAttributesForm(url, is_response_reseted, category_selector, 
 	}
 }
 
-function load_combo(id_combo, options, selected_option) {
+function load_combo(id_combo, options, selected_option, text_to_display) {
 	$(id_combo).children().remove();
-	if (options != undefined) {
-		if (selected_option == undefined) {
+	if (options != undefined)
+	{
+		if (selected_option == undefined)
+		{
 			selected_option = options[0];
 		}
-		for (var i = 0; i<options.length; i++) {
+		for (var i = 0; i<options.length; i++)
+		{
 			$(id_combo).append(new Option(options[i].label, options[i].id, selected_option.id == options[i].id, selected_option.id == options[i].id));
 			$(id_combo).parents(".form-group:first").show();
 		}
-	} else {
+	} 
+	else if (selected_option != undefined && (selected_option.id == '-1' || selected_option == -1))
+	{
+			$(id_combo).append(new Option((selected_option.label != undefined ? selected_option.label : text_to_display), -1));
+			$(id_combo).parents(".form-group:first").show();
+	} 
+	else
+	{
 		$(id_combo).parents(".form-group:first").hide();
 	}
 }
