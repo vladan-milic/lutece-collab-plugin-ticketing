69100decd2042e1f08b1ff965b445e6b2ddc7985
Jamel <jamel.abassou@accenture.com>
2017-12-21 15:00:29 +0100

    TICKETING-349 : Back Office - Setting the display of a form
    add update script sql

---
+++ src/sql/plugins/ticketing/upgrades/update_db_ticketing-1.3.8-2.0.0.sql
@@ -0,0 +1,17 @@
+-- Structure for table ticketing_viewing 
+-- 
+ 
+DROP TABLE IF EXISTS ticketing_viewing; 
+CREATE TABLE ticketing_viewing ( 
+id_viewing int(6) NOT NULL, 
+title varchar(50) default '', 
+message varchar(5000) default '', 
+buton_label varchar(50) default '', 
+channel varchar(50) default '', 
+contact_mode varchar(50) default '', 
+civility varchar(50) default '', 
+domain varchar(50) default '', 
+thematic varchar(50) default '', 
+location varchar(50) default '', 
+PRIMARY KEY (id_viewing) 
+);
