1357edbf6d7d7bfe486dd188eb3344170513bf40
bisouduperou <stephane.raynaud@accenture.com>
2018-01-05 14:04:22 +0100

    TICKETING-364 : Give RBAC access to categories instead of domains

--- src/sql/plugins/ticketing/plugin/create_db_ticketing.sql
+++ src/sql/plugins/ticketing/plugin/create_db_ticketing.sql
@@ -95,7 +95,7 @@ id_category_type int(6) NOT NULL default '0',
 id_workflow int(6) NOT NULL default '0',
 demand_id int(6) NOT NULL default '0',
 help_message varchar(500) NULL,
-is_manageable int(1) NOT NULL DEFAULT '0',
+is_manageable int(1) DEFAULT 0 NOT NULL,
 inactive int(1)  NOT NULL default '0',
 PRIMARY KEY (id_category)
 );
