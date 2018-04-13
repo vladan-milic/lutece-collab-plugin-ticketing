bcab2f0bf0b6197efdd53af22ed2cd9ad38668c1
killianMorvan <killian.morvan@accenture.com>
2018-04-13 10:28:18 +0200

    TICKETING 400 change script, mistake in the columns name

--- src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.25.0-2.26.0.sql
+++ src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.25.0-2.26.0.sql
@@ -1,3 +1,3 @@
 -- Change name
 ALTER TABLE `ticketing_model_reponses`
-	CHANGE COLUMN `id_ticket_category` `label_ticket_domain` VARCHAR(50) NULL DEFAULT '' COLLATE 'utf8_unicode_ci' AFTER `keyword`; 
+	CHANGE COLUMN `label_ticket_domain` `id_ticket_category` VARCHAR(50) NULL DEFAULT '' COLLATE 'utf8_unicode_ci' AFTER `keyword`; 
