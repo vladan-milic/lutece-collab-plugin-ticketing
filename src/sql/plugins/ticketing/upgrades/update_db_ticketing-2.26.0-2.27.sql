763080b76558d47ac62987574f8f1e7eb53f76ae
bisouduperou <stephane.raynaud@accenture.com>
2018-04-24 11:17:29 +0200

    TICKETING-405 - Subject of e-mail should be a parameter in the task

--- src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.26.0-2.27.sql
+++ src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.26.0-2.27.sql
@@ -1,1 +1,4 @@
 update ticketing_category set demand_id=101;
+
+ALTER TABLE workflow_task_ticketing_email_external_user_config ADD default_subject varchar(255) NULL;
+ALTER TABLE workflow_ticketing_email_external_user ADD email_subject varchar(255) NULL ;
