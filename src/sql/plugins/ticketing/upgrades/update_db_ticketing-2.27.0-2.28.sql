34d0c3cab9e3e8de2639aacc70a27ed88a56d0c3
vladan.milic <vladan.milic@MW7QNUWNJLCL65.dir.svc.accenture.com>
2018-05-18 11:59:39 +0200

    TICKETING-414  Show Date Of Last Modification Of A Typical Response

--- src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.27.0-2.28.sql
+++ src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.27.0-2.28.sql
@@ -1,9 +1,9 @@
 /***
  * SCRIPT TABLE TICKETIN MODEL RESPONSES ADD COLUMNS - BEGIN
  */
-ALTER TABLE ticketing.ticketing_model_reponses ADD date_update varchar(100) DEFAULT '' NOT NULL ;
-ALTER TABLE ticketing.ticketing_model_reponses ADD firstname varchar(100) DEFAULT '' NOT NULL ;
-ALTER TABLE ticketing.ticketing_model_reponses ADD lastname varchar(100) DEFAULT '' NOT NULL ;
+ALTER TABLE ticketing_model_reponses ADD date_update varchar(100) DEFAULT '' NOT NULL ;
+ALTER TABLE ticketing_model_reponses ADD firstname varchar(100) DEFAULT '' NOT NULL ;
+ALTER TABLE ticketing_model_reponses ADD lastname varchar(100) DEFAULT '' NOT NULL ;
 /***
  * SCRIPT TABLE TICKETIN MODEL RESPONSES ADD COLUMNS - END
  */
