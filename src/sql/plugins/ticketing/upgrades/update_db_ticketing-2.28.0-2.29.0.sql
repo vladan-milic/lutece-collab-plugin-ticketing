eb33a1dc2af4b653dddc8b94bb11a0b7eb338b14
vladan.milic <vladan.milic@CPX-APUIX1QGW46.dir.svc.accenture.com>
2018-10-29 10:31:54 +0100

    TICKETING-436 - ARCHIVE ATTACHED PIECES

--- src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.28.0-2.29.0.sql
+++ src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.28.0-2.29.0.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `ticketing_file_blob` (
 	`id_file_blob` INT(11) NOT NULL,
+	`creation_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	`id_file` INT(11) NOT NULL,
 	`id_blob` VARCHAR(50) NOT NULL,
 	PRIMARY KEY (`id_file_blob`)
