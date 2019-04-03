001882369fd7272b92845981cb568b81d0c59126
vladan.milic <vladan.milic@CPX-APUIX1QGW46.dir.svc.accenture.com>
2019-04-03 18:28:51 +0200

    fix script

--- src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.26.0-2.27.sql
+++ src/sql/plugins/ticketing/upgrades/update_db_ticketing-2.26.0-2.27.sql
@@ -1,9 +1,10 @@
-update ticketing_category set demand_id=101;
-
 ALTER TABLE workflow_task_ticketing_email_external_user_config ADD default_subject varchar(255) NULL;
 ALTER TABLE workflow_ticketing_email_external_user ADD email_subject varchar(255) NULL ;
 
 ALTER TABLE `ticketing_ticket` ADD COLUMN `demand_id` INT(6) NOT NULL DEFAULT '0' AFTER `id_marking`;
+
+update ticketing_category set demand_id=101;
+
 update ticketing_ticket a inner join ticketing_category b on a.id_ticket_category = b.id_category set a.demand_id = b.demand_id;
 
 
