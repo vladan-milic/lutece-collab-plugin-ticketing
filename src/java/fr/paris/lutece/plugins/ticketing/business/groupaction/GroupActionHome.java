ded04a605dd6a23b3c6c662b7d6f30c5c9b956ff
geoffroy huet <geoffroy.huet@accenture.com>
2020-06-30 11:57:31 +0200

    Gestion groupes

--- src/java/fr/paris/lutece/plugins/ticketing/business/groupaction/GroupActionHome.java
+++ src/java/fr/paris/lutece/plugins/ticketing/business/groupaction/GroupActionHome.java
@@ -1,3 +1,36 @@
+/*
+ * Copyright (c) 2002-2020, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
 package fr.paris.lutece.plugins.ticketing.business.groupaction;
 
 import java.util.List;
@@ -108,6 +141,9 @@ public class GroupActionHome
      */
     public static GroupAction update( GroupAction groupAction )
     {
+        GroupAction oldGroupAction = findByPrimaryKey( groupAction.getIdGroup( ) );
+        changeOrder( groupAction, oldGroupAction.getOrdre( ) );
+
         _dao.store( groupAction, _plugin );
 
         return groupAction;
@@ -121,7 +157,33 @@ public class GroupActionHome
      */
     public static void remove( int nKey )
     {
+        GroupAction oldGroupAction = findByPrimaryKey( nKey );
+
+        if ( oldGroupAction != null )
+        {
+            updateOrderOnRemove( oldGroupAction.getOrdre( ) );
+        }
         _dao.delete( nKey, _plugin );
+    }
+
+    /**
+     * Update order on remove.
+     *
+     * @param nOrderId
+     *            the n order id
+     */
+    private static void updateOrderOnRemove( int nOrderId )
+    {
+        for ( GroupAction groupActionChange : getGroupActionList( ) )
+        {
+            int nGroupActionToUpdateOrder = groupActionChange.getOrdre( );
+
+            if ( ( nGroupActionToUpdateOrder > nOrderId ) )
+            {
+                groupActionChange.setOrdre( nGroupActionToUpdateOrder - CONSTANT_STEP_ORDER );
+                _dao.store( groupActionChange, _plugin );
+            }
+        }
     }
 
     /**
